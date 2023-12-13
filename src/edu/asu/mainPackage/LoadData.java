7e267a612d18775e971134dd52a87fb04bef93eb
adham667 <adhamsaeed667@gmail.com>
2023-12-13 13:13:13 +0200

    added_files

--- src/edu/asu/mainPackage/LoadData.java
+++ src/edu/asu/mainPackage/LoadData.java
@@ -1,7 +1,31 @@
 package edu.asu.mainPackage;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public interface LoadData {
     default void loadData(){
+        File users = new File("users.txt");
+        File flights = new File("Flights.txt");
+        try{
+            ObjectInputStream userInputStream = new ObjectInputStream(new FileInputStream(users));
+            WorkFlow.appUsers=(ArrayList<ApplicationUser>) userInputStream.readObject();
+        }catch (ClassNotFoundException e) {
+            System.out.println("File Not found");
+            try {
+                users.createNewFile();
+            }catch(IOException exp){
+                System.out.println(exp);
+            }
+        }catch (IOException e){
+            System.out.println(e.toString());
+        }
 
+        try{
+            ObjectOutputStream flightsOutputStream = new ObjectOutputStream(new FileOutputStream(flights));
+            flightsOutputStream.writeObject(WorkFlow.Flights);
+        }catch (IOException ioException){
+            System.out.println(ioException.toString());
+        }
     }
 }
--- src/edu/asu/mainPackage/UploadData.java
+++ src/edu/asu/mainPackage/UploadData.java
@@ -1,7 +1,26 @@
 package edu.asu.mainPackage;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public interface UploadData {
     default void uploadData(){
+        File users = new File("users.txt");
+        File flights = new File("Flights.txt");
+        try{
+            ObjectOutputStream userOutputStream = new ObjectOutputStream(new FileOutputStream(users));
+            userOutputStream.writeObject(WorkFlow.appUsers);
+        }catch (IOException ioException){
+            System.out.println(ioException.toString());
+        }
 
+        try{
+            ObjectOutputStream flightsOutputStream = new ObjectOutputStream(new FileOutputStream(flights));
+            flightsOutputStream.writeObject(WorkFlow.Flights);
+        }catch (IOException ioException){
+            System.out.println(ioException.toString());
+        }
     }
 }
